f99a47048aa3a930e87e340e5b001ad133d58374
Daniel Pincay <alexispincay005@gmail.com>
2022-03-07 17:54:29 -0500

    empzando vista a pagina resumen

---
+++ components/Resumen/Concesionarios.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full mt-3">
+      <div class="flex justify-center">
+          <select class="form-select form-select-lg mb-3
+      block
+      w-4/5
+      px-4
+      py-2
+      text-xl
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding bg-no-repeat
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-gray-800 focus:outline-none">
+        <option selected disabled>Select an Agency..</option>
+    </select>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return{
+            
+        }
+    },
+    apollo:{
+
+    }
+}
+</script>
+
--- pages/products/resumen/_id.vue
+++ pages/products/resumen/_id.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>hola</div>
+  <div  class="flex  justify-center mt-3">
+    <div class="flex flex-wrap w-5/6 " v-if="vehiculoSelected.id">
+      <div class="w-full sm:w-1/2 mb-3  px-1">
+        <img class="w-full max-h-full" :src="RouteServer+vehiculoSelected.url_imagen.url" alt="vehiculo Seleccionado">
+        <div>
+           <h1 class="text-white bg-gray-800 text-center font-bold text-2xl">RESUMEN</h1>
+        </div>
+      </div>
+      <div class="w-full sm:w-1/2 mb-3 px-1">
+           <h1 class="text-white bg-gray-800 text-center font-bold text-2xl">ECOGE UN CONCESIONARIO</h1>
+           <Concesionarios />
+      </div>
+  </div>
+  </div>
 </template>
 
 <script>
+import Concesionarios from '~/components/Resumen/Concesionarios.vue'
+import { mapGetters } from "vuex";
 export default {
-
-}
+  mounted() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position.coords);
+    });
+  },
+  created() {
+    if(process.client){
+      if(!this.vehiculoSelected.id && localStorage.getItem("vehiculoSelected")){
+        this.$store.commit("setVehiculoSelected", JSON.parse(localStorage.getItem("vehiculoSelected")))
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(["vehiculoSelected"]),
+    RouteServer() {
+      return process.env.baseUrl;
+    },
+  },
+  components:{
+    Concesionarios
+  }
+};
 </script>
 
 <style>
-
 </style>
