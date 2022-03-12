107bc4a9dc8ef3e509b69dd8d3bffd87c08f56b6
AnthonyDelValle <aadvl382016@outlook.com>
2022-03-11 21:18:24 -0500

    Update cartComponent.vue

--- components/cart/cartComponent.vue
+++ components/cart/cartComponent.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <div v-for="(item, index) in getCart" :key="index">
-            <p>{{item.vehiculo.modelo}} --> $ {{item.precio}}</p>
-            <p>{{item}}</p>
+        <div v-for="(item, index) in getCart" :key="index" >
+            <p v-on="cargar(item, index)">-----------------</p>
+            <p v-if="item.vehiculo">{{item.vehiculo.modelo}} -> {{item.precio}}</p>
+            <p v-if="item.especificacion">{{item.especificacion}} -> {{item.precio}}</p>
+            <p v-if="item.tipotransmision">{{item.tipotransmision.tipo}} -> {{item.precio}}</p>
+            <p v-if="item.tipotraccion">{{item.tipotraccion.tipo}} -> {{item.precio}}</p>
+            <p v-if="item.name">{{item.name}} -> {{item.precio}}</p>
         </div>
     </div>
 </template>
@@ -11,6 +15,20 @@ import { mapGetters} from "vuex";
 
 export default {
     name: "CartComponent",
+    data() {
+        return {
+            contador:0,
+        }
+    },
+    methods:{
+        cargar(algo, numero){
+            console.log("Mostrando"+numero);
+            console.log(algo);
+            if(numero==4){
+                console.log(algo.name)
+            }
+        },
+    },
     computed:{
         ...mapGetters(["getCartTotal","getCart"]),
     }
